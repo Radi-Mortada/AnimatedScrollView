4f639f3d7285bf3e6d7d039ca783a7f0972efbb0
Radi <radi.mortada@malsapp.com>
2017-05-02 20:31:26 +0200

    animated Scroll

---
+++ App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  Animated,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+var BLACK = 0;
+var RED = 1;
+var BLUE = 2;
+
+
+export default class ScrollableHeader extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: new Animated.Value(0),
+      fadeAnim: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(  // Animate over time
+      this.state.fadeAnim,  // The animated value to drive
+      {
+        toValue: 100,  // Animate to opacity: 1, or fully opaque
+      }).start();  // Starts the animation
+  }
+
+  _renderScrollViewContent() {
+    const data = Array.from({ length: 10 });
+    return (
+      <View style={styles.scrollViewContent}>
+        {data.map((_, i) =>
+          <View key={i} style={styles.row}>
+            <Text>{i}</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp',
+    });
+
+    var bgColor = this.state.scrollY.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['rgba(56, 45, 108, 1)', 'rgba(255, 255, 255, 0)']
+    });
+
+    return (
+      <View style={styles.fill}>
+        <Animated.View style={[styles.header, { backgroundColor: bgColor, height: headerHeight }]}>
+          <View style={{ height: 100, marginHorizontal:10, backgroundColor:'red' }}>
+            <Text style={styles.title}>Title</Text>
+          </View>
+        </Animated.View>
+        <ScrollView
+          style={styles.fill}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }]
+          )}
+        >
+          {this._renderScrollViewContent()}
+        </ScrollView>
+      </View >
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Metrics, ApplicationStyles, Fonts } from '../../Themes/'
+import I18n from 'react-native-i18n'
+
+export default StyleSheet.create({
+    header: {
+        position: 'absolute',
+        top: 20,
+        left: 0,
+        right: 0,
+        backgroundColor: 'black',
+    },
+    bar: {
+        marginTop: 100,
+        height: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: 'white',
+        fontSize: 18,
+    },
+    scrollViewContent: {
+        marginTop: HEADER_MAX_HEIGHT,
+    },
+
+})
